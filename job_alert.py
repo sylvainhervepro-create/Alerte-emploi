1bd7d2b1953c3fd8c49d2000b5bfda4c8bb4e462
sylvainhervepro-create <sylvainhervepro@gmail.com>
2026-05-26 16:14:44 +0200

    Update job_alert.py

--- job_alert.py
+++ job_alert.py
@@ -123,7 +123,7 @@ def send_email(jobs):
 # ── Main ────────────────────────────────────────────────────────
 if __name__ == "__main__":
     token = get_ft_token()
-    print(f"✅ Token obtenu : {token[:10]}...")  # ← ajouter cette ligne
+    print(f"✅ Token obtenu : {token[:10]}...")
     jobs  = fetch_jobs(token)
     send_email(jobs)
     jobs  = fetch_jobs(token)
